3432d4e5462cb6621f41362edcbdbbca90a3d43c
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2023-10-29 03:07:47 -0700

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Header from "./components/Header";
-import './App.css';
+import LandingSection from "./components/LandingSection";
+import ProjectsSection from "./components/ProjectsSection";
+import ContactMeSection from "./components/ContactMeSection";
+import Footer from "./components/Footer";
+import { AlertProvider } from "./context/alertContext";
+import Alert from "./components/Alert";
+
 
 function App() {
   return (
