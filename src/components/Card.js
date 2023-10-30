bd9a29fbc81f22865aaffc8dd07d7b2b94181c71
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2023-10-30 11:53:44 -0700

    modified Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,17 +3,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
-const Card = ({ title, description, imageSrc }) => {
+const Card = withTheme(({ title, description, imageSrc, theme }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return(
-    <div >
-    <Image src={imageSrc}/>
-    <Heading>{title}</Heading>
-    <Text>{description}</Text>
-  </div>
+  return (
+    <VStack color="black" 
+    backgroundColor={theme.colors.white} 
+    borderRadius="xl">
+      <Image src={imageSrc} borderRadius="xl" cursor={"pointer"}/>
+        <VStack spacing={4} p={4} alignItems="flex-start">
+          <HStack justifyContent="space-between" 
+          alignItems="center">
+            <Heading as="h3" size="md">
+              {title}
+            </Heading>
+            <Text color={theme.colors.gray["500"]} fontSize="lg">
+              {description}
+            </Text>
+          </HStack>
+          <HStack backgroundColor={theme.colors.gray["100"]} borderRadius="xl" 
+          cursor={"pointer"}>
+            <p>See more</p>
+            <FontAwesomeIcon icon={faArrowRight} size="1x"/>
+          </HStack>
+        </VStack>
+    </VStack>
   );
-};
+});
+
 
 export default Card;
